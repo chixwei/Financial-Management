b2160a2713441f75eaf9fe6fd750c58bf658c1a5
chixwei <78296446+chixwei@users.noreply.github.com>
2021-09-13 01:04:32 +0800

    login and sign up
    can login and sign up

--- app/src/main/java/com/example/financialmanagement/Login.java
+++ app/src/main/java/com/example/financialmanagement/Login.java
@@ -1,14 +1,91 @@
 package com.example.financialmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    Button signup_button, loginButton;
+    EditText loginEmail, loginPassword;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //assign variable
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+
+        //login Button
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(loginEmail.getText().toString().isEmpty()) {
+                    loginEmail.setError("Please Enter Your Email");
+                    return;
+                }
+
+                if(loginPassword.getText().toString().isEmpty()) {
+                    loginPassword.setError("Please Enter Your Password");
+                    return;
+                }
+
+                //login user
+                firebaseAuth.signInWithEmailAndPassword(loginEmail.getText().toString(), loginPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+        //go to sign up button
+        signup_button = findViewById(R.id.signupPage);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //If login before no need to login again
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
     }
 }
+
+
--- app/src/main/java/com/example/financialmanagement/SignUp.java
+++ app/src/main/java/com/example/financialmanagement/SignUp.java
@@ -1,14 +1,120 @@
 package com.example.financialmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
+
+    ImageButton back_button;
+    EditText signupUname, signupEmail, signupPassword, signupRePassword;
+    Button signupButton;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //assign variable
+        signupUname = findViewById(R.id.signupUname);
+        signupEmail = findViewById(R.id.signupEmail);
+        signupPassword = findViewById(R.id.signupPassword);
+        signupRePassword = findViewById(R.id.signupRePassword);
+
+        //sign up button
+        signupButton = findViewById(R.id.signupButton);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //extract the data from the form
+                String Uname = signupUname.getText().toString();
+                String Email = signupEmail.getText().toString();
+                String Password = signupPassword.getText().toString();
+                String RePassword = signupRePassword.getText().toString();
+
+                //Error if empty
+                if(Uname.isEmpty()) {
+                    signupUname.setError("Username is Required");
+                    return;
+                }
+
+                if(Email.isEmpty()) {
+                    signupEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(Password.isEmpty()) {
+                    signupPassword.setError("Password is Required");
+                    return;
+                }
+
+                if(RePassword.isEmpty()) {
+                    signupRePassword.setError("Please Re-enter Your Password");
+                    return;
+                }
+
+                //2 password must same
+                if(!Password.equals(RePassword)) {
+                    signupRePassword.setError("Password Do Not Match");
+                    return;
+                }
+
+                //Data is valid
+                //Register User
+
+                Toast.makeText(SignUp.this, "Data Validated", Toast.LENGTH_SHORT).show();
+
+                //Firebase
+                fAuth = FirebaseAuth.getInstance();
+                //If Success
+                fAuth.createUserWithEmailAndPassword(Email, Password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //Go into Main Page
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        finish();
+
+                    }
+
+                    //If Fail
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+
+        //back button
+        back_button = findViewById(R.id.backButton);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
+

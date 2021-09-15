de95383a77d726f808d5a945f6329449cb66c048
chixwei <78296446+chixwei@users.noreply.github.com>
2021-09-16 00:58:11 +0800

    navigation bar
    Fragment and toolbar not yet complete

--- app/src/main/java/com/example/financialmanagement/MainActivity.java
+++ app/src/main/java/com/example/financialmanagement/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.financialmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    ImageButton menuButton;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variables
+    static final float END_SCALE = 0.7f;
+
+    //Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuButton;
+    ConstraintLayout contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +35,98 @@ public class MainActivity extends AppCompatActivity {
 
         //Menu Button
         menuButton = findViewById(R.id.menuButton);
+
+        //Menu Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        contentView = findViewById(R.id.content);
+
+        navigationDrawer();
+
+
+    }
+
+
+    //Navigation Drawer Functions
+    private void navigationDrawer() {
+
+        //Hide or show items
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+
+
+        //Navigation Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                startActivity(new Intent(getApplicationContext(), Menu.class));
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        animateNavigationDrawer();
+    }
+
+    private void animateNavigationDrawer() {
+
+        //Can Change color with this code :
+        //drawerLayout.setScrimColor(Color.rgb(158,227,255));
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
             }
         });
     }
+
+    //back button will close the navigation if opened, not close the whole app
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                break;
+
+            case R.id.nav_income:
+                Intent income = new Intent(MainActivity.this, Income.class);
+                startActivity(income);
+
+            case R.id.nav_expenses:
+                Intent expenses = new Intent(MainActivity.this, Expenses.class);
+                startActivity(expenses);
+        }
+
+        //if choose one to open then will close drawer
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 }

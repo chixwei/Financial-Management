b9f112244ac890a5fe11e9752bc2468036f450c1
vmarkeu <71437369+vmarkeu@users.noreply.github.com>
2021-09-17 02:39:40 +0800

    welcome

---
+++ app/src/main/java/com/example/financialmanagement/Welcome3.java
@@ -0,0 +1,51 @@
+package com.example.financialmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Welcome3 extends AppCompatActivity {
+
+    ImageView back_button;
+    Button login_button, signup_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome3);
+
+        // back button
+        back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome3.this, Welcome2.class);
+                startActivity(intent);
+            }
+        });
+
+        // login button
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome3.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        // signup button
+        signup_button = findViewById(R.id.signup_button);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome3.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

c1125aeafd5925d171bd7884b1bbcfe9cc120727
chixwei <78296446+chixwei@users.noreply.github.com>
2021-09-19 16:21:38 +0800

    forget password done

--- app/src/main/java/com/example/financialmanagement/Profile.java
+++ app/src/main/java/com/example/financialmanagement/Profile.java
@@ -1,18 +1,39 @@
 package com.example.financialmanagement;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_profile, container, false);
+        View view = inflater.inflate(R.layout.activity_profile, container, false);
+
+        ImageView logout = view.findViewById(R.id.logout_button);
+        logout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(),Welcome3.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
+
 }

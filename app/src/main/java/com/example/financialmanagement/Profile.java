fc139df07871f2d941f91bd48d6d5c2df96520ec
vmarkeu <71437369+vmarkeu@users.noreply.github.com>
2021-09-21 04:06:13 +0800

    logout

--- app/src/main/java/com/example/financialmanagement/Profile.java
+++ app/src/main/java/com/example/financialmanagement/Profile.java
@@ -1,39 +1,69 @@
 package com.example.financialmanagement;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends Fragment {
 
+    ImageView logout_button;
+    Dialog dialog;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_profile, container, false);
 
-        ImageView logout = view.findViewById(R.id.logout_button);
-        logout.setOnClickListener(new View.OnClickListener() {
+        dialog = new Dialog(view.getContext());
 
+        // logout button
+        logout_button = view.findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getActivity(),Welcome3.class);
-                startActivity(intent);
+                dialog.setContentView(R.layout.logout);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+                // logout back button
+                Button logout_back_button = dialog.findViewById(R.id.logout_back_button);
+                logout_back_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                // confirm logout button
+                Button confirm_logout_button = dialog.findViewById(R.id.confirm_logout_button);
+                confirm_logout_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(getActivity(), Welcome3.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
 
         return view;
     }
-
 }

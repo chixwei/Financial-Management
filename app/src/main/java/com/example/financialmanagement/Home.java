e592ba0ab861e7d434ef28ed79f760e98dca911c
vmarkeu <71437369+vmarkeu@users.noreply.github.com>
2021-09-22 23:41:30 +0800

    database

--- app/src/main/java/com/example/financialmanagement/Home.java
+++ app/src/main/java/com/example/financialmanagement/Home.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -21,6 +22,9 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_home, container, false);
+
+        // test database
+        Toast.makeText(getActivity(),"Firebase connection successful", Toast.LENGTH_SHORT).show();
 
         // define text
         add_expense_text = v.findViewById(R.id.add_expense_text);
